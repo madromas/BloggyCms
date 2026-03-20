91b9f816eab1252a67471bfe14183e7239bfd58d
pechoradev <pechora-dev@yandex.ru>
2026-03-21 02:49:14 +0300

    Удаление игнорируемых файлов из Git

--- system/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-            define('DB_HOST', 'localhost');
-            define('DB_NAME', 'bloggy4');
-            define('DB_USER', 'bloggy4');
-            define('DB_PASS', 'Fside2014$$');
-            define('DB_PREFIX', 'bc_');
-            define('DB_CHARSET', 'utf8mb4');
-            define('DB_COLLATE', 'utf8mb4_unicode_ci');
